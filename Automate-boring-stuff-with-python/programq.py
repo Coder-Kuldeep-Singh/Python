f7334010551e55d88baa16406693ad1de1162d7d
mavensingh <kuldeep.avsar@gmail.com>
2019-12-14 09:49:45 -0800

    new file

---
+++ Automate-boring-stuff-with-python/programq.py
@@ -0,0 +1,10 @@
+# This programs says hello  and asks for my name
+print('This is Python')
+print('What is your name?') # ask for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('This length of your name is:')
+print(len(myName))
+print('What is your age?') # ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
