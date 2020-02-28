6195d09baa20cb014b789d3616bb7738273653da
mavensingh <kuldeep.avsar@gmail.com>
2020-02-28 02:44:02 -0800

    learning

---
+++ mastering_python/basics-of-python/string-formatting.py
@@ -0,0 +1,17 @@
+age = 23
+print("My age is {0} years".format(age))
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6}, {7}".format(31,"january","March","May","July","August","October","December"))
+
+print("""January : {2}
+February : {0}
+March : {2}
+April : {1}
+May : {2}
+June : {1}
+July : {2}
+August : {2}
+September : {1}
+October : {2}
+November : {1}
+December : {2}
+""".format(28,30,31))
